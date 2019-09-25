dc64f304683c740fc839ec0778c3031e4fddaa81
maxim <maximmahnev@gmail.com>
2019-09-25 16:27:40 +0700

    Добавьте файлы проекта.

---
+++ Switch/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Switch
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        App()
+        {
+            InitializeComponent();
+        }
+
+        [STAThread]
+        static void Main()
+        {
+            App app = new App();
+            //MainWindow window = new MainWindow();
+            Global gl = new Global();
+            gl.Run();
+            app.Run(gl.view);
+            
+        }
+    }
+}
---
+++ Switch/Models/Global.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch
+{
+    public class Global
+    {
+        public Views.MainWindow view;
+
+        public void Run()
+        {
+            view = new Views.MainWindow();
+            Console.WriteLine("HW");
+        }
+    }
+}
